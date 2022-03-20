82dfb06157f4010c7fd74e998b82ea356d80293f
Viacheslav <surzhenkoviacheslav@gmail.com>
2022-03-20 16:07:39 +0200

    FPS Limit

--- main.cpp
+++ main.cpp
@@ -6,14 +6,28 @@ const int SCREEN_HEIGHT = 480;
 Game* game = nullptr;
 
 int main(int argc, char* args[]) {
-    game = new Game();
 
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    Uint32 frameStart;
+    int frameTime;
+
+    game = new Game();
     game->Init("SellBro Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, false);
 
     while(game->Running()){
+        frameStart = SDL_GetTicks();
+
         game->HandleEvents();
         game->Update();
         game->Render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if(frameDelay >= frameTime){
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
 
     game->Clean();
